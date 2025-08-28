9a481ca7eefc1be7e062a6fa5fd892ab1420ac12
berezuma <59442488+berezuma@users.noreply.github.com>
2025-08-28 17:36:21 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -35,7 +35,7 @@ const App: React.FC = () => {
 
 	const [ucMeta, setUcMeta] = React.useState<UCMeta>({
 		compress: false,
-		extension: 'jpg',
+		extension: 'jpeg',
 	})
 
 	const uploadOnChange = React.useCallback(
